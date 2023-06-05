13d33d5400c7c504e118f2c1a5f8091cd1bc3b65
arfhsanim <ariful.cse37@gmail.com>
2023-06-05 14:59:18 +0600

    f

---
+++ home.php
@@ -0,0 +1,42 @@
+<!-- <h1><?php echo $title;?></h1> -->
+
+<div class="col-lg-12">  
+<h4>Computer Science & Engineering </h4>
+    <p> Name of Course: Software Engineering </p>
+    <p>Course Code: CSE 313 </p> 
+</div>
+<div  class="col-lg-6">
+  <h4>Text Book</h4> 
+  <p>1 Sommerville, I. (2004). Software Engineering. International computer science series. (9 th Edition).</p>
+  
+  <h4>Reference</h4><ol>
+    <li>Pressman, R. S. (2005). Software engineering: a practitioner's approach.Palgrave Macmillan.</li>  
+    <li>Software Engineering, Roger S. Pressman (7 th edition)</li> 
+    <li> Introduction to Java Programming, Comprehensive Version, Y. Daniel Liang,</li> 
+  </ol>
+  
+  <h4>Course Rationale</h4> 
+  <p>Software Engineering is one of the fundamental courses for CSE students. It aims to
+give students the practical idea about software engineering and analytical skills to use
+in their subsequent course work and professional development. Discusses several
+methods and principles of quality assurance and concept on professional software
+development. <br>
+The course is designed to provide the background of the following topics:
+<br> <br>
+a) To provide a comprehensive introduction to software engineering leading to
+the ability to understand contemporary terminology, progress, issues, and
+trends. <br>
+b) A thorough introduction to software engineering techniques, focusing on
+software process models, agile development. Principle that guides practice,
+understanding requirements, Requirements modeling: scenarios, information
+and analysis classes, Requirements modeling: Flow, behavior, patterns and
+web apps, Design concept, pattern-based design, quality management,
+software quality assurance, software testing strategies. <br>
+c) The interdisciplinary nature of software engineering is emphasized in the wide
+variety of examples and applications. To build up logical thinking.
+</p>
+
+
+
+ 
+
